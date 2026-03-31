ec4b188d4a9fddce1495b1b52b4a91b3ff4ed246
othertrexzor <bertalan.jobbagy@gmail.com>
2026-03-31 23:26:59 +0200

    SPA React
    SPA React (spa.html) oldal hozzáadva.
    összes html-ben az elérési utak frissítve vannak. 1-1 React alakalmazás megcsinálva. 1 tic-tac-toe és 1 coin flip

---
+++ react-spa/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TicTacToe from './tictactoe/tictactoe'
+import FlipCoin from './coinflip/FlipCoin'
+
+
+import './App.css'
+import './tictactoe/css/ttt.css'
+
+
+function App() {
+
+  const [activePage, setActivePage] = useState();
+
+  return (
+
+    <>
+      <Header />
+      <main>
+        <section className='glass-panel hero'>
+          <h2>React alkalmazások</h2>
+          <div className="spa-menu">
+            <button onClick={() => setActivePage("tictactoe")}>
+              Tic-Tac-Toe
+            </button>
+            <button onClick={() => setActivePage("flipcoin")}>
+              Flip Coin
+            </button>
+          </div>
+          <div style={{ marginTop: "2rem" }}>
+            {activePage === "flipcoin" && <FlipCoin />}
+            {activePage === "tictactoe" && <TicTacToe />}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+
+  )
+}
+
+export default App
---
+++ react-spa/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <header>
+        <h1>Web programozás-1 Előadás Házi feladat</h1>
+        <nav>
+            <a href="../../index.html">Főoldal</a>
+            <a href="../../javascript.html">Csapatok listája (JS CRUD)</a>
+            <a href="../../react.html">Játékosok (React CRUD)</a>
+            <a href="./index.html" className="active">SPA React</a>
+        </nav>
+    </header>
+    );
+
+}
+
+export default Header;
